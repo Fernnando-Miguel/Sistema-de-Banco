45e98f91189d5737cb1fe57f2159ec70555fb976
Fernanndo Miguel <fernnandomiguel@gmail.com>
2026-05-13 17:34:47 -0300

    Menu funcional criado

--- main.c
+++ main.c
@@ -18,25 +18,70 @@ typedef struct {
         printf("Saldo: %f\n", usuario.saldo);
     }
 
-int main () {
-    Conta usuario;
+    void criar_conta(Conta *usuario){
 
-    printf("===CADASTRO===\n");
+        printf("===CADASTRO===\n");
     printf("Digite seu nome: ");
-    fgets(usuario.nome, 90, stdin);
+    fgets(usuario->nome, 90, stdin);
 
     printf("Digite seu cpf: ");
-    scanf("%d", &usuario.cpf);
+    scanf("%d", &usuario->cpf);
 
     printf("Digite seu numero da conta: ");
-    scanf("%d", &usuario.numero_conta);
+    scanf("%d", &usuario->numero_conta);
 
     while(getchar() != '\n' );
 
     printf("Digite sua senha: ");
-    fgets(usuario.senha, 50, stdin);
+    fgets(usuario->senha, 50, stdin);
 
-      mostrar_conta(usuario);
+    }
+
+int main () {
+
+    Conta usuario; 
+    int opcao;
+
+    do {
+
+    printf("\n ===MENU===\n");
+    printf("1 - Cadastrar\n");
+    printf("2 - Mostrar usuario cadastrado\n");
+    printf("0 - Sair\n");
+    
+    printf("Escolha uma opcao: ");
+    scanf("%d", &opcao);
+    while(getchar() != '\n');
+    
+    switch(opcao){
+
+        case 1:
+
+        criar_conta(&usuario);
+
+        break;
+
+        case 2: 
+
+        mostrar_conta(usuario);
+
+        break;
+
+        case 0:
+
+        printf("Saindo...");
+
+        break;
+
+        default:
+
+        printf("Opcao invalida!");
+
+        break;
+
+    }
+
+    }while (opcao != 0);
 
     return 0;
 }
